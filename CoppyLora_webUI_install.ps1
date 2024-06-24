c2485a8aee1f3adb2f760e6180e6a64b9b157ea4
tori29umai <72191117+tori29umai0123@users.noreply.github.com>
2024-06-24 17:11:51 +0900

    Update CoppyLora_webUI_install.ps1

--- CoppyLora_webUI_install.ps1
+++ CoppyLora_webUI_install.ps1
@@ -29,19 +29,30 @@ pip install -U bitsandbytes
 pip install gradio==3.41.2
 pip install pyinstaller
 
-# ファイルを置換
-$filePath = "library\lpw_stable_diffusion.py"
+# 定義されたファイルパス
+$filePath1 = "library\lpw_stable_diffusion.py"
+$filePath2 = "library\sdxl_lpw_stable_diffusion.py"
+
+# 置き換える古い行と新しい行
 $oldLine1 = "from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput, StableDiffusionSafetyChecker"
 $newLine1 = "from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput"
 $newLine2 = "from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker"
 
-if ((Get-Content $filePath) -match $oldLine1) {
-    (Get-Content $filePath) -replace $oldLine1, ($newLine1 + "`n" + $newLine2) | Set-Content $filePath
-    Write-Output "File has been modified."
+# ファイル1の処理
+if ((Get-Content $filePath1) -match $oldLine1) {
+    (Get-Content $filePath1) -replace $oldLine1, ($newLine1 + "`n" + $newLine2) | Set-Content $filePath1
+    Write-Output "File $filePath1 has been modified."
 } else {
-    Write-Output "No modification needed."
+    Write-Output "No modification needed for $filePath1."
 }
 
+# ファイル2の処理
+if ((Get-Content $filePath2) -match $oldLine1) {
+    (Get-Content $filePath2) -replace $oldLine1, ($newLine1 + "`n" + $newLine2) | Set-Content $filePath2
+    Write-Output "File $filePath2 has been modified."
+} else {
+    Write-Output "No modification needed for $filePath2."
+}
 cd ..
 
 Write-Output "Installation completed"
