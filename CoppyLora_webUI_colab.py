43f4c18444d7cf8e20df5b8feaba7ef2080013e0
tori29umai <72191117+tori29umai0123@users.noreply.github.com>
2024-11-05 14:57:00 +0900

    Update CoppyLora_webUI_colab.py

--- CoppyLora_webUI_colab.py
+++ CoppyLora_webUI_colab.py
@@ -648,6 +648,7 @@ def detail_train(base_model, detail_lora_name, detail_base_img_path, detail_base
         "ratios": [1.41, -1.41],
         "concat": True,
         "shuffle": True,
+        "lbws": [],        
     }
     args = argparse.Namespace(**args_dict)   
     merge.merge(args)
@@ -662,6 +663,7 @@ def detail_train(base_model, detail_lora_name, detail_base_img_path, detail_base
         "verbose": "store_true",
         "dynamic_param": None,
         "dynamic_method": None,
+        "lbws": [],        
     }
     args = argparse.Namespace(**args_dict)  
     resize.resize(args)
